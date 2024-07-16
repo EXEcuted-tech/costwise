adecf0cfce5a03823b064532503da7ec1a4814c7
Katteu <katheamari@gmail.com>
2024-07-16 16:09:17 +0800

    chore: update npm dependencies

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -8,11 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        'poppins': ['Poppins','sans-serif'],
       },
+      colors:{
+        primary: '#B22222',
+        secondary: '#FEF200'
+      },
+      screens: {
+        'xl': '1024px',
+        '2xl': '1280px',
+        '3xl': '1440px',
+        '4xl': '1536px'
+      }
     },
   },
   plugins: [],
