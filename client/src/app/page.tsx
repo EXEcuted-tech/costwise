b00789f46925cb4850a25d499511b3c56a21d151
henneh <galvehannah5302@gmail.com>
2024-07-21 20:36:23 +0800

    chore: testing page cleanup

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,17 +1,7 @@
-"use client"
-import { SidebarProvider } from "@/context/SidebarContext";
-import AccountCreation from "./user-management/create/AccountCreation";
-import MainLayout from "@/components/layouts/MainLayout";
-
 export default function Home() {
   return (
-    <SidebarProvider>
-      <div className="flex">
-        <MainLayout/>
-        <main>
-        <AccountCreation/>
-        </main>
+      <div>
+       Test
       </div>
-    </SidebarProvider>
   );
 }
