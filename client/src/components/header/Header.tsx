8260fa9cd556d8fd1de9a6972a1d7ffc883863e0
Katteu <katheamari@gmail.com>
2024-07-21 17:23:54 +0800

    fix: resolved header width issue

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -13,16 +13,16 @@ const Header: React.FC<HeaderProps> = ({icon: Icon, title}) => {
     return (
         <div className='font-lato bg-white w-full h-[5rem] flex shadow-md shadow-gray-300'>
             <div className="flex items-center w-full">
-                <div className="w-[5rem] p-6 ml-1 text-[3em]">
+                <div className="w-[5rem] pl-9 py-6 ml-1 text-[3em]">
                     <Icon />
                 </div>
-                <div className="w-[40rem] text-[2em] m-2 p-[0.6rem] font-bold ">
+                <div className="w-[40rem] text-[2.3em] mx-4 mt-2 pr-[0.6rem] py-[0.6rem] font-black ">
                     {title}
                 </div>
 
                 {/* change this to proper dark mode */}
-                <div className="p-6 text-[2.2em] ml-auto mr-2">
-                    <MdDarkMode />
+                <div className="p-6 text-[2.2em] text-primary ml-auto mr-4">
+                    <MdDarkMode className='cursor-pointer'/>
                 </div>
             </div>
         </div>
