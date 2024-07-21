b3eb8e27e4b038aadcb851120dcf0d1115a664f9
henneh <galvehannah5302@gmail.com>
2024-07-21 13:50:06 +0800

    fix: sizing

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -11,17 +11,17 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({icon: Icon, title}) => {
 
     return (
-        <div className='font-lato bg-white w-full h-[6rem] flex shadow-md shadow-gray-300'>
+        <div className='font-lato bg-white w-full h-[5rem] flex shadow-md shadow-gray-300'>
             <div className="flex items-center w-full">
-                <div className="w-[5rem] p-6 ml-2 text-[3.5em] ">
+                <div className="w-[5rem] p-6 ml-1 text-[3em]">
                     <Icon />
                 </div>
-                <div className="w-[40rem] text-[2.3em] m-2 p-[0.6rem] font-bold ">
+                <div className="w-[40rem] text-[2em] m-2 p-[0.6rem] font-bold ">
                     {title}
                 </div>
 
                 {/* change this to proper dark mode */}
-                <div className="p-6 text-[2.5em] ml-auto mr-2">
+                <div className="p-6 text-[2.2em] ml-auto mr-2">
                     <MdDarkMode />
                 </div>
             </div>
