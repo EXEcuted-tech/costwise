61ac116c8e1bd2233ecbf39073c85329f1b4c1d6
Katteu <katheamari@gmail.com>
2024-07-21 12:38:22 +0800

    fix: changed hotdog button from position relative to fixed

--- client/src/components/layouts/MainLayout.tsx
+++ client/src/components/layouts/MainLayout.tsx
@@ -14,7 +14,7 @@ function MainLayout() {
       >
         {isOpen ? <OpenSidebar /> : <CloseSidebar />}
       </div>
-      <div className='relative top-[400px] bg-[#DD8383] flex flex-col justify-center h-[125px] w-[45px] rounded-r-3xl hover:w-[55px] cursor-pointer transition-all duration-300 ease-in-out'
+      <div className={`fixed  ${isOpen ? 'left-[360px]' : 'left-[120px]'} top-[400px] bg-[#DD8383] flex flex-col justify-center h-[125px] w-[45px] rounded-r-3xl hover:w-[55px] cursor-pointer transition-all duration-200 ease-in-out`}
           onClick={()=>setIsOpen(!isOpen)}>
         <div className='flex justify-center items-center'>
           <Image src={hotdog} alt={'Hotdog Icon'} className='w-[35px] h-auto object-cover' />
