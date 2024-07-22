7627fbda8320f5be36a4b84fd927adf612e4d804
PonPonc <gybaez321@gmail.com>
2024-07-22 08:19:03 +0800

    staged changes to image responsiveness

--- client/src/app/logout/page.tsx
+++ client/src/app/logout/page.tsx
@@ -7,30 +7,30 @@ function LogoutPage() {
         <img
           src="images/statsimage.png"
           alt="Logout Image"
-          className="w-[90%] h-auto"
+          className="h-auto max-h-[95%]"
         />
       </div>
 
       <div className="bg-green h-[40%] w-[100%] flex-row items-center justify-center text-white">
-        <div className="flex items-center justify-center h-[40%] w-[100%]">
-          <p className="text-[4em] md:text-[4.5em] lg:text-[5em] font-semibold">
+        <div className="flex items-center justify-center h-[40%] w-[100%] max-h-[80%]">
+          <p className="text-[6vh] md:text-[7vh] lg:text-[8vh] font-semibold">
             Logged Out!
           </p>
         </div>
         <div className="flex items-center justify-center h-[10%] w-[100%] font-poppins">
-          <p className="text-[1.2em] md:text-[1.4em] lg:text-[1.6em]">
+          <p className="text-10 sm:text-[1.2rem] md:text-[1.4em] lg:text-[1.6em]">
             Thank you for using CostWise!
           </p>
         </div>
         <div className="flex items-center justify-center h-[10%] w-[100%] font-poppins">
-          <p className="text-[1.2em] md:text-[1.4em] lg:text-[1.6em]">
+          <p className="text-10 sm:text-[1.2em] md:text-[1.4em] lg:text-[1.6em]">
             To access the page, please log in again.
           </p>
         </div>
         <div className="flex items-center justify-center h-[30%] w-[100%] font">
           <div className="bg-secondary w-[50%] flex items-center justify-center h-[60%] rounded-3xl">
             <Link href="/login">
-              <button className="text-[1.2em] md:text-[1.4em] lg:text-[1.6em] text-[#A60000] font-bold">
+              <button className="text-[1em] sm:text-[1.2em] md:text-[1.4em] lg:text-[1.6em] text-[#A60000] font-bold">
                 Return to login Page
               </button>
             </Link>
