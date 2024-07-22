5f0fc3764e2857eed4449c8bfa750f665f690ae5
FranzGwapo <franzcasimir.ondiano@gmail.com>
2024-07-22 08:04:46 +0800

    fix: added xl and 4xl responsiveness

--- client/src/app/audit-log/page.tsx
+++ client/src/app/audit-log/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { FaFilter } from "react-icons/fa";
 import { LuArrowDownUp, LuArrowUpDown } from "react-icons/lu";
 import { FaFileExport } from "react-icons/fa6";
+import { BiSearchAlt } from "react-icons/bi";
 import { LayoutProps } from "@/context/SidebarContext";
 import { useSidebarContext } from "@/context/SidebarContext";
 
@@ -15,43 +16,46 @@ return (
     <div className={`w-full font-lato mt-[50px]`}>
         <div className="flex justify-between w-[80%] m-auto mb-[10px] gap-5">
             <div className="flex gap-5">
-                <input className="p-[10px] border border-[#868686] rounded-full w-[250px]" placeholder="Search here..." />
-                <button className="flex justify-center items-center p-[10px] border border-[#868686] rounded-[10px] w-[60px]">
-                    <LuArrowDownUp className="text-[25px] text-[#414141]" />
+                <div className="relative">
+                    <BiSearchAlt className="absolute left-3 top-[20px] 4xl:top-[28px] text-[20px] 4xl:text-[23px] transform -translate-y-1/2 text-gray-600"/>
+                    <input className="p-[6px] 4xl:p-[10px] text-[16px] 4xl:text-[20px] pl-[35px] 4xl:pl-[40px] border border-[#868686] rounded-full w-[200px] 4xl:w-[250px]" placeholder="Search here..." />
+                </div>
+                <button className="flex justify-center items-center 4xl:p-[10px] border border-[#868686] rounded-[10px] h-[40px] 4xl:h-[52px] w-[50px] 4xl:w-[60px]">
+                    <LuArrowDownUp className="text-[20px] 4xl:text-[25px] text-[#414141]" />
                 </button>
-                <button className="flex justify-center items-center px-[10px] border border-[#868686] rounded-[10px] w-[60px]">
-                    <FaFilter className="text-[25px] text-[#414141]" />
+                <button className="flex justify-center items-center 4xl:p-[10px] border border-[#868686] rounded-[10px] h-[40px] 4xl:h-[52px] w-[50px] 4xl:w-[60px]">
+                    <FaFilter className="text-[20px] 4xl:text-[25px] text-[#414141]" />
                 </button>
             </div>
-            <button className="flex text-[20px] p-[10px] border border-[#868686] rounded-[10px] justify-end gap-2">
-                <FaFileExport className="text-[30px] text-[#414141]" />
+            <button className="flex text-[16px] 4xl:text-[20px] p-[6px] 4xl:p-[10px] h-[40px] 4xl:h-[52px] border border-[#868686] rounded-[10px] justify-end gap-2">
+                <FaFileExport className="text-[25px] 4xl:text-[30px] text-[#414141]" />
                 Export
             </button>
         </div>
         <div className="w-[80%] m-auto">
-            <div className="flex justify-between m-auto text-[22px] p-[20px] bg-primary text-white font-bold rounded-t-[20px]">
-                <p>Timestamp</p>
-                <p className="ml-[70px]">
+            <div className="flex text-[18px] 4xl:text-[22px] p-[15px] 4xl:p-[20px] bg-primary text-white font-bold rounded-t-[20px]">
+                <p className="w-[28%]">Timestamp</p>
+                <p className="w-[28%]">
                     Employee No.
                     </p>
-                <p className="mr-[30px]">
+                <p className="w-[31.5%] 4xl:w-[34%]">
                     Action/Event
                 </p>
-                <p className="mr-[27px]">
+                <p>
                     Action
                 </p>
             </div>
-            <div className="flex justify-between m-auto text-[18px] px-[20px] py-[10px] border border-black border-opacity-[50%] border-t-0">
-                <div>
+            <div className="flex w-full text-[14px] 4xl:text-[18px] pl-[15px] 4xl:pl-[20px] py-[7px] 4xl:py-[10px] border border-black border-opacity-[50%] border-t-0">
+                <div className="w-[27.5%]">
                     <p>January 12, 2024 12:50:22</p>
                 </div>
-                <div>
+                <div className="w-[23.5%]">
                     <p>#112391</p>
-                    <p className="text-[16px] opacity-[50%]">
+                    <p className="text-[12px] opacity-[50%]">
                         Regular User
                     </p>
                 </div>
-                <div className="flex text-[16px] items-center">
+                <div className="flex w-[34.5%] 4xl:w-[37.5%] text-[12px] 4xl:text-[16px] items-center">
                     <p>Record changed: BOM_V1_Cost.csv</p>
                 </div>
                 <div className="flex items-center">
@@ -63,17 +67,17 @@ return (
                     </p>
                 </div>
             </div>
-            <div className="flex justify-between m-auto text-[18px] px-[20px] py-[10px] border border-black border-opacity-[50%] border-t-0">
-                <div>
+            <div className="flex w-full text-[14px] 4xl:text-[18px] pl-[15px] 4xl:pl-[20px] py-[7px] 4xl:py-[10px] border border-black border-opacity-[50%] border-t-0">
+                <div className="w-[27.5%]">
                     <p>January 12, 2024 12:50:22</p>
                 </div>
-                <div>
+                <div className="w-[23.5%]">
                     <p>#112391</p>
-                    <p className="text-[16px] opacity-[50%]">
+                    <p className="text-[12px] opacity-[50%]">
                         Regular User
                     </p>
                 </div>
-                <div className="flex text-[16px] items-center">
+                <div className="flex w-[34.5%] 4xl:w-[37.5%] text-[12px] 4xl:text-[16px] items-center">
                     <p>Record changed: BOM_V1_Cost.csv</p>
                 </div>
                 <div className="flex items-center">
@@ -81,20 +85,21 @@ return (
                         onClick={toggleDrawer} 
                         className="cursor-pointer font-bold text-primary"
                     >
-                        Show More</p>
+                        Show More
+                    </p>
                 </div>
             </div>
-            <div className="flex justify-between m-auto text-[18px] px-[20px] py-[10px] border border-black border-opacity-[50%] border-t-0 rounded-b-[20px]">
-                <div>
+            <div className="flex w-full text-[14px] 4xl:text-[18px] pl-[15px] 4xl:pl-[20px] py-[7px] 4xl:py-[10px] border border-black border-opacity-[50%] border-t-0 rounded-b-[20px]">
+                <div className="w-[27.5%]">
                     <p>January 12, 2024 12:50:22</p>
                 </div>
-                <div>
+                <div className="w-[23.5%]">
                     <p>#112391</p>
-                    <p className="text-[16px] opacity-[50%]">
+                    <p className="text-[12px] opacity-[50%]">
                         Regular User
                     </p>
                 </div>
-                <div className="flex text-[16px] items-center">
+                <div className="flex w-[34.5%] 4xl:w-[37.5%] text-[12px] 4xl:text-[16px] items-center">
                     <p>Record changed: BOM_V1_Cost.csv</p>
                 </div>
                 <div className="flex items-center">
@@ -102,7 +107,8 @@ return (
                         onClick={toggleDrawer} 
                         className="cursor-pointer font-bold text-primary"
                     >
-                        Show More</p>
+                        Show More
+                    </p>
                 </div>
             </div>
         </div>
