2e0a65de7d1653111fd1e531ef3e13e4c8ff3459
Katteu <katheamari@gmail.com>
2024-07-25 04:26:42 +0800

    fix: adjusted responsiveness of 1024px until 1290px screens

--- client/src/components/layouts/MainLayout.tsx
+++ client/src/components/layouts/MainLayout.tsx
@@ -16,7 +16,7 @@ const MainLayout = () => {
       >
         {isOpen ? <OpenSidebar /> : <CloseSidebar />}
       </div>
-      <div className={`fixed  ${isOpen ? 'left-[360px]' : 'left-[120px]'} top-[400px] bg-[#DD8383] flex flex-col justify-center h-[125px] w-[45px] rounded-r-3xl hover:w-[55px] cursor-pointer transition-all duration-200 ease-in-out`}
+      <div className={`fixed  ${isOpen ? 'left-[280px] 2xl:left-[360px]' : 'left-[120px]'} top-[400px] bg-[#DD8383] flex flex-col justify-center h-[125px] w-[45px] rounded-r-3xl hover:w-[55px] cursor-pointer transition-all duration-200 ease-in-out`}
         onClick={() => setIsOpen(!isOpen)}>
         <div className='flex justify-center items-center'>
           <Image src={hotdog} alt={'Hotdog Icon'} className='w-[35px] h-auto object-cover' />
