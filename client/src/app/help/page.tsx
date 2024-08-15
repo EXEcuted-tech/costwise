0561d91cafddcc136852c06eb229c3502af1c911
PonPonc <gybaez321@gmail.com>
2024-08-15 10:06:19 +0800

    feat(web): saving changes on subtopics in help page

--- client/src/app/help/page.tsx
+++ client/src/app/help/page.tsx
@@ -9,9 +9,11 @@ const UserManualPage = () => {
   const divShadows = {
     boxShadow: 'inset 0 0 10px rgba(0, 0, 0, 0.3)',
   }
-
+  const divTopShadow = {
+    boxShadow: 'inset 0px 10px 10px rgba(0, 0, 0, 0.3)',
+  }
   return (
-    <div className='overflow-hidden bg-cover bg-center items-center justify-center shadow-2xl bg-[#FFC24B] bg-opacity-20' style={{ backgroundImage: "url('/images/usermanbg.png')" }}>
+    <div className='overflow-hidden bg-cover bg-center items-center justify-center bg-[#FFC24B] bg-opacity-20' style={{ backgroundImage: "url('/images/usermanbg.png')" }}>
       <div>
         <Header icon={PiBookOpenText} title="User's Management"></Header>
       </div>
@@ -41,17 +43,17 @@ const UserManualPage = () => {
 
         </div>
         {/* Bottom Div */}
-        <div className='flex flex-row h-[45%] w-[105%] bg-[#FFFAF8] items-center justify-center px-5 overflow-b-hidden z-10'>
+        <div className='flex flex-row h-[45%] w-[105%] bg-[#FFFAF8] items-center justify-center px-5 overflow-b-hidden z-10' style={divTopShadow}>
           <div className='flex  w-[33.33%] h-[120%] items-start justify-end '>
             <div className='flex flex-row h-[75%] bg-white w-[80%] rounded-xl  px-5 py-5 ' style={divShadows}>
-              <div className='flex w-[20%] items-start justify-center'>
-                <FaCompass className='text-[30px]' />
+              <div className='flex w-[20%] 2xl:w-[10%] items-start justify-center'>
+                <FaCompass className='text-[30px] 2xl:text-[40px] 3xl:text-[50px]' />
               </div>
               <div className='flex flex-col pl-[5%] w-[80%] text-tertiary'>
-                <div className='text-[16px] font-bold'><p>Getting Started</p></div>
-                <div className='text-[14px] pt-[3%]'><p>Learn how to use Product Costing System.</p></div>
-                <div className='flex flex-col h-10% pl-[15%] pt-[10%]'>
-                  <ul className='list-disc'>
+                <div className='text-[16px] 2xl:text-[20px] 3xl:text-[24px] font-bold'><p>Getting Started</p></div>
+                <div className='text-[14px] 2xl:text-[16px] 3xl:text-[20px] pt-[3%]'><p>Learn how to use Product Costing System.</p></div>
+                <div className='flex flex-col h-10% pl-[15%] 2xl:pl-[20px] pt-[10%] 3xl:pt-[10px] '>
+                  <ul className='list-disc 2xl:text-[16px] 3xl:text-[20px]'>
                     <li>
                       Lorem ipsum
                     </li>
@@ -76,13 +78,13 @@ const UserManualPage = () => {
           <div className='flex  w-[33.33%] h-[120%] items-start justify-center '>
             <div className='flex flex-row h-[75%] bg-white w-[80%] rounded-xl  px-5 py-5 ' style={divShadows}>
               <div className='flex w-[20%] items-start justify-center'>
-                <CgProfile className='text-[30px]' />
+                <CgProfile className='text-[30px] 2xl:text-[40px] 3xl:text-[50px]' />
               </div>
               <div className='flex flex-col pl-[5%] w-[80%] text-tertiary'>
-                <div className='text-[16px] font-bold'><p>Getting Started</p></div>
-                <div className='text-[14px] pt-[3%]'><p>Learn how to use Product Costing System.</p></div>
-                <div className='flex flex-col h-10% pl-[15%] pt-[10%]'>
-                  <ul className='list-disc'>
+                <div className='text-[16px] 2xl:text-[20px] 3xl:text-[24px] font-bold'><p>Getting Started</p></div>
+                <div className='text-[14px] 2xl:text-[16px] 3xl:text-[20px] pt-[3%]'><p>Learn how to use Product Costing System.</p></div>
+                <div className='flex flex-col h-10% pl-[15%] 2xl:pl-[20px] pt-[10%] 3xl:pt-[10px] '>
+                  <ul className='list-disc 2xl:text-[16px] 3xl:text-[20px]'>
                     <li>
                       Lorem ipsum
                     </li>
@@ -107,13 +109,13 @@ const UserManualPage = () => {
           <div className='flex w-[33.33%] h-[120%]'>
             <div className='flex flex-row h-[75%] bg-white w-[80%] rounded-xl px-5 py-5' style={divShadows}>
               <div className='flex w-[20%] items-start justify-center'>
-                <GrHelpBook className='text-[30px]' />
+                <GrHelpBook className='text-[30px] 2xl:text-[40px] 3xl:text-[50px]' />
               </div>
               <div className='flex flex-col pl-[5%] w-[80%] text-tertiary'>
-                <div className='text-[16px] font-bold'><p>Getting Started</p></div>
-                <div className='text-[14px] pt-[3%]'><p>Learn how to use Product Costing System.</p></div>
-                <div className='flex flex-col h-10% pl-[15%] pt-[10%]'>
-                  <ul className='list-disc'>
+                <div className='text-[16px] 2xl:text-[20px] 3xl:text-[24px] font-bold'><p>Getting Started</p></div>
+                <div className='text-[14px] 2xl:text-[16px] 3xl:text-[20px] pt-[3%]'><p>Learn how to use Product Costing System.</p></div>
+                <div className='flex flex-col h-10% pl-[15%] 2xl:pl-[20px] pt-[10%] 3xl:pt-[10px] '>
+                  <ul className='list-disc 2xl:text-[16px] 3xl:text-[20px]'>
                     <li>
                       Lorem ipsum
                     </li>
@@ -137,7 +139,7 @@ const UserManualPage = () => {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
