353fa034e73da6b7f199a67e5494e4e510a2dbc9
FranzGwapo <franzcasimir.ondiano@gmail.com>
2024-08-27 12:46:35 +0800

    fix: converted div to table and responsiveness

--- client/src/app/audit-log/page.tsx
+++ client/src/app/audit-log/page.tsx
@@ -27,14 +27,22 @@ const { drawerOpen, setDrawerOpen } = useDrawerContext();
 const { isOpen, setIsOpen } = useSidebarContext();
 const toggleDrawer = () => setDrawerOpen(!drawerOpen);
 const [ data, setData ] = useState<AuditTableProps[]>(fakeAuditAllData);
-console.log(fileData);
+
+const [currentPage, setCurrentPage] = useState<number>(1);
+const handlePageChange = (e: React.ChangeEvent<unknown>, page: number) => {
+    setCurrentPage(page);
+};
+
+const indexOfLastItem = currentPage * 8;
+const indexOfFirstItem = indexOfLastItem - 8;
+const currentListPage = data ? data.slice(indexOfFirstItem, indexOfLastItem) : [];
 
 return (
     <div className={`w-full h-screen font-lato bg-background`}>
         { drawerOpen ? <AuditDrawer /> : "" }
         <Header icon={GoHistory} title={"Audit Log"} />
         
-        <div className="flex justify-between w-[80%] m-auto mb-[10px] gap-5 pt-[50px]">
+        <div className="flex justify-between w-[80%] m-auto mb-[10px] gap-5 pt-[45px]">
             <div className="flex gap-5">
                 <div className="relative">
                     <BiSearchAlt className="absolute left-3 top-[20px] 4xl:top-[24px] text-[20px] 4xl:text-[23px] transform -translate-y-1/2 text-gray-600"/>
@@ -52,41 +60,49 @@ return (
                 Export
             </button>
         </div>
-        <div className="w-[80%] m-auto">
-                <div className="flex text-[18px] 4xl:text-[22px] p-[15px] 4xl:p-[20px] bg-primary text-white font-bold rounded-t-[20px]">
-                    <p className="w-[28%]">Timestamp</p>
-                    <p className="w-[28%]">Employee No.</p>
-                    <p className="w-[31.5%] 4xl:w-[34%]">Action/Event</p>
-                    <p>Action</p>
-                </div>
-                {data && data.map((data, index) => (
-                    <div key={index} className={`flex w-full text-[14px] 4xl:text-[18px] pl-[15px] 4xl:pl-[20px] py-[7px] 4xl:py-[10px] border border-black border-opacity-[50%]`}>
-                        <div className="w-[27.5%]">
-                            <p>{data.dateTimeAdded}</p>
-                        </div>
-                        <div className="w-[23.5%]">
-                            <p>{data.employeeNo}</p>
-                            <p className="text-[12px] opacity-[50%]">{data.userType}</p>
-                        </div>
-                        <div className="flex w-[34.5%] 4xl:w-[37.5%] text-[12px] 4xl:text-[16px] items-center">
-                            <p>{data.actionEvent}</p>
-                        </div>
-                        <div className="flex items-center">
-                            <p onClick={() => {}} className="cursor-pointer font-bold text-primary">
+        {/* Table */}
+        <table className="w-[80%] m-auto">
+            <thead className="flex text-[18px] 4xl:text-[22px] p-[15px] 4xl:p-[20px] bg-primary text-white font-bold rounded-t-[20px] text-left">
+                <th className="w-[28%]">Timestamp</th>
+                <th className="w-[27.5%]">Employee No.</th>
+                <th className="w-[31.5%] 4xl:w-[34.3%]">Action/Event</th>
+                <th>Action</th>
+            </thead>
+            <tbody>
+                {currentListPage.length > 0 ? currentListPage.map((data, index) => (
+                    <tr key={index} className={`flex w-full text-[14px] 4xl:text-[18px] pl-[15px] 4xl:pl-[20px] py-[7px] 4xl:py-[10px] border border-black border-opacity-[50%] ${index === currentListPage.length - 1 ? 'rounded-b-xl' : ''}`}>
+                        <tr className="w-[27.5%]">
+                            <td>{data.dateTimeAdded}</td>
+                        </tr>
+                        <tr className="flex flex-col w-[23.5%]">
+                            <td>{data.employeeNo}</td>
+                            <td className="text-[14px] 4xl:text-[16px] opacity-[50%]">{data.userType}</td>
+                        </tr>
+                        <tr className="flex w-[34.5%] 4xl:w-[37.5%] text-[14px] 4xl:text-[16px] items-center">
+                            <td>{data.actionEvent}</td>
+                        </tr>
+                        <tr className="flex items-center">
+                            <td onClick={toggleDrawer} className="cursor-pointer font-bold text-primary">
                                 Show More
-                            </p>
-                        </div>
+                            </td>
+                        </tr>
+                    </tr>
+                )): 
+                (
+                    <div className="flex justify-center border border-black border-opacity-[50%] rounded-b-xl py-[7px] 4xl:py-[10px]">
+                        <p>No logs available</p>
                     </div>
-                ))}
-            </div>
-        {/* <div className='relative py-[1%]'>
+                )}
+            </tbody>
+        </table>
+        <div className='relative py-[1%]'>
             <PrimaryPagination
-                data={fileData}
+                data={data}
                 itemsPerPage={8}
                 handlePageChange={handlePageChange}
                 currentPage={currentPage}
             />
-        </div> */}
+        </div>
     </div>
   )
 }
@@ -94,6 +110,66 @@ return (
 export default AuditLogPage
 
 const fakeAuditAllData: AuditTableProps[] = [
+    {
+        dateTimeAdded: 'January 12, 2024 12:50:22',
+        employeeNo: '#112391',
+        userType: 'Regular User',
+        actionEvent: 'Record changed: BOM_V1_Cost.csv'
+    },
+    {
+        dateTimeAdded: 'January 13, 2024 12:50:22',
+        employeeNo: '#123531',
+        userType: 'Regular User',
+        actionEvent: 'Record changed: BOM_V3_Cost.csv'
+    },
+    {
+        dateTimeAdded: 'January 14, 2024 12:50:22',
+        employeeNo: '#125131',
+        userType: 'Regular User',
+        actionEvent: 'Record changed: BOM_V5_Cost.csv'
+    },
+    {
+        dateTimeAdded: 'January 15, 2024 12:50:22',
+        employeeNo: '#199999',
+        userType: 'Regular User',
+        actionEvent: 'Record changed: BOM_V7_Cost.csv'
+    },
+    {
+        dateTimeAdded: 'January 17, 2024 12:50:22',
+        employeeNo: '#188888',
+        userType: 'Regular User',
+        actionEvent: 'Record changed: BOM_V9_Cost.csv'
+    },
+    {
+        dateTimeAdded: 'January 12, 2024 12:50:22',
+        employeeNo: '#112391',
+        userType: 'Regular User',
+        actionEvent: 'Record changed: BOM_V1_Cost.csv'
+    },
+    {
+        dateTimeAdded: 'January 13, 2024 12:50:22',
+        employeeNo: '#123531',
+        userType: 'Regular User',
+        actionEvent: 'Record changed: BOM_V3_Cost.csv'
+    },
+    {
+        dateTimeAdded: 'January 14, 2024 12:50:22',
+        employeeNo: '#125131',
+        userType: 'Regular User',
+        actionEvent: 'Record changed: BOM_V5_Cost.csv'
+    },
+    {
+        dateTimeAdded: 'January 15, 2024 12:50:22',
+        employeeNo: '#199999',
+        userType: 'Regular User',
+        actionEvent: 'Record changed: BOM_V7_Cost.csv'
+    },
+    {
+        dateTimeAdded: 'January 17, 2024 12:50:22',
+        employeeNo: '#188888',
+        userType: 'Regular User',
+        actionEvent: 'Record changed: BOM_V9_Cost.csv'
+    },
     {
         dateTimeAdded: 'January 12, 2024 12:50:22',
         employeeNo: '#112391',
