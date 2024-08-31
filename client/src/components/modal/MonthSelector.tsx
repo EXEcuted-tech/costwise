0538330c4ddbdf097dc448b844c0107c86ac1a63
Katteu <katheamari@gmail.com>
2024-08-31 15:10:06 +0800

    fix: modal placement error fixed

--- client/src/components/modal/MonthSelector.tsx
+++ client/src/components/modal/MonthSelector.tsx
@@ -25,7 +25,7 @@ const MonthSelector:React.FC<ModalProps> = ({months, onClose, onMonthSelect}) =>
         
     };
     return (
-        <div className={`${isOpen ? 'left-52 4xl:left-52 3xl:left-52 2xl:left-52 xl:left-40' : 'left-20'} flex justify-center items-center w-full h-full fixed top-0 left-0 backdrop-brightness-50 z-[1000]`}>
+        <div className={`flex items-center justify-center w-full h-full top-0 left-0 fixed backdrop-brightness-50 z-[1000]`}>
             <div className={`${isOpen ? '4xl:w-[860px] 3xl:w-[820px] 2xl:w-[800px] xl:w-[550px]' : '4xl:w-[860px] 3xl:w-[820px] 2xl:w-[800px] xl:w-[550px]'} px-[35px] mx-[50px] 2xl:mx-0 animate-pop-out bg-white w-[860px] min-h-[210px] mt-[-50px] rounded-[10px]`}>
                 <div className='flex justify-between pt-[10px] mb-2'>
                     <div className='flex items-center'>
