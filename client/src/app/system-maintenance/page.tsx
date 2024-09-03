6684b61c173958d75181ebbbcf9c02187968a45f
henneh <galvehannah5302@gmail.com>
2024-09-03 15:02:02 +0800

    feat: added main system maintenance page

---
+++ client/src/app/system-maintenance/page.tsx
@@ -0,0 +1,146 @@
+"use client";
+import React, { useState } from 'react';
+import Header from '@/components/header/Header';
+import { useSidebarContext } from '@/context/SidebarContext';
+import PrimaryPagination from '@/components/pagination/PrimaryPagination';
+import { GrSystem } from "react-icons/gr";
+import { AiFillDatabase } from "react-icons/ai";
+import { PiFilesFill } from "react-icons/pi";
+import { IoGitNetworkOutline } from "react-icons/io5";
+import { IoMdAdd, IoIosSearch } from "react-icons/io";
+import { TfiMoreAlt } from "react-icons/tfi";
+
+export interface SystemMaintenanceProps {
+    date: String;
+    title: String;
+    autor: String;
+};
+
+const SystemMaintenance = () => {
+    const { isOpen } = useSidebarContext();
+    const [currentPage, setCurrentPage] = useState<number>(1);
+
+    const handlePageChange = (e: React.ChangeEvent<unknown>, page: number) => {
+        setCurrentPage(page);
+    };
+
+
+    // const indexOfLastItem = currentPage * 8;
+    // const indexOfFirstItem = indexOfLastItem - 8;
+    // const currentListPage = fileData.slice(indexOfFirstItem, indexOfLastItem);
+
+    return (
+        <div className='w-full'>
+            <div>
+                <Header icon={GrSystem} title="System Maintenance" />
+            </div>
+
+            <div className='bg-yellow-50'>
+                {/* Info Tiles */}
+                <div className='flex mx-16 mt-12 mb-9 px-[15rem] '>
+                    {/* Database Storage */}
+                    <div className='flex flex-col p-4 w-[30rem] h-[17rem] mr-[5rem] bg-white rounded-lg drop-shadow-lg'>
+                        <div className='flex flex-col mb-8'>
+                            <div className='flex'>
+                                <span className='text-[38px] mr-2'>128</span>
+                                <span className='text-[30px] pt-2'>GB</span>
+                                <AiFillDatabase className='text-[60px] ml-auto mr-3 text-gray-200'/>
+                            </div>
+                            <span className='text-[23px] font-light'>Database Storage</span>
+                        </div>
+
+                        <div className='flex flex-col'>
+                            <div className='w-full h-[1.5rem] bg-gray-100 rounded-full'>
+                                <div className='w-[50%] h-[1.5rem] bg-[#DD8383] rounded-full'></div>
+                            </div>
+                            <span className='text-[17px] pt-1 pl-1 font-light'>300mb used</span>
+                        </div>
+
+                        <button className='w-[9rem] text-[16px] mt-6 border border-[#9290905b] font-light bg-white shadow-lg rounded-lg'>View Database</button>
+                    </div>
+
+                    {/* Files */}
+                    <div className='flex flex-col p-4 w-[30rem] h-[17rem] bg-white rounded-lg drop-shadow-lg'>
+                        <div className='flex flex-col mb-8'>
+                            <div className='flex'>
+                                <span className='text-[38px] mr-2'>50</span>
+                                <span className='text-[30px] pt-2'>FILES</span>
+                                <PiFilesFill className='text-[60px] ml-auto mr-3 text-gray-200'/>
+                            </div>
+                            <span className='text-[23px] font-light'>Total No. of Files</span>
+                        </div>
+
+                        <div className='flex flex-col'>
+                            <div className='flex w-full h-[1.5rem] bg-gray-100 rounded-full'>
+                                <div className='w-[20%] h-[1.5rem] bg-[hsl(0,57%,69%)] rounded-l-full'></div>
+                                <div className='w-[30%] h-[1.5rem] bg-[#9EE29E]'></div>
+                                <div className='w-[30%] h-[1.5rem] bg-[#B6CDFF] rounded-r-full'></div>
+                            </div>
+                            <div className='flex flex-col p-3'>
+                                <div className='flex'>
+                                    <div className='w-4 h-4 mt-1 mr-2 bg-[#DD8383] rounded-full'></div>
+                                    <span className='text-[17px] font-light'>Material-Cost</span>
+                                </div>
+                                <div className='flex'>
+                                    <div className='w-4 h-4 mt-1 mr-2 bg-[#9EE29E] rounded-full'></div>
+                                    <span className='text-[17px] font-light'>Transactions</span>
+                                </div>
+                                <div className='flex'>
+                                    <div className='w-4 h-4 mt-1 mr-2 bg-[#B6CDFF] rounded-full'></div>
+                                    <span className='text-[17px] font-light'>Inventory</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Release Notes */}
+                <div className='flex flex-col h-[30rem] mx-16 bg-white rounded-lg drop-shadow-md'>
+                    {/* Header */}
+                    <div className='flex w-full h-[4rem] px-4 py-3 font-medium bg-white border border-[#9290906c] drop-shadow-md rounded-t-lg'>
+                        <IoGitNetworkOutline className='text-[32px] text-gray-500 mt-1 mr-3' />
+                        <span className='text-[28px] text-[#5B5353]'>Release Notes</span>
+                        <button className='w-[2rem] h-[2rem] text-[25px] ml-5 mt-1 px-[3px] text-gray-500 border border-[#9290905b] bg-white drop-shadow-md rounded-lg'><IoMdAdd /></button>
+                        <div className="mt-[3px] ml-auto text-gray-600">
+                            <div className='flex absolute text-[1.3em] text-gray-400 mt-[0.3rem] ml-3'>
+                                <IoIosSearch />
+                            </div>
+                            <input
+                            
+                                className={`${isOpen ? 'w-[20rem] 2xl:w-[25rem] 3xl:w-[30rem]' : 'w-[20rem]'} bg-white h-8 px-5 pl-9 text-[1.1em] border border-[#9290906c] rounded-lg focus:outline-none`}
+                                type="search"
+                                name="search"
+                                placeholder="Search here..."
+                            />
+                        </div>
+                    </div>
+
+                    {/* Main Content Area */}
+                    <div className='w-full h-[23rem] p-6 px-16 bg-blue-50'>
+                        <div className='flex w-full h-[5rem] items-center bg-white drop-shadow-lg rounded-lg'>
+                            <div className='flex w-[8rem] h-full px-10 font-semibold text-[20px] text-[#5B5353] text-center items-center border-r-2 border-[#92909041] bg-[#f1f1f1a1] '>
+                                <span className=''>15 Jan</span>
+                            </div>
+                            <span className='px-10 text-[23px] text-[#5B5353]'>Automated reports</span>
+                            <span className='mr-3 text-[21px] text-[#5B5353] font-light'>by: </span>
+                            <span className='text-[21px] text-[#5B5353] font-light'>Michael Huang</span>
+                            <button className='w-[4rem] h-[2rem] text-[30px] text-[#5B5353] px-4 py-[3px] ml-auto mr-10 bg-white border border-[#9290905b] rounded-full shadow-lg'><TfiMoreAlt/></button>
+                        </div>
+                    </div>
+
+                    {/* Footer
+                    <div className="flex w-full justify-center h-[86px] rounded-b-xl border-[#868686]">
+                        <PrimaryPagination
+                            data={InventoryFakeData} //change to data
+                            itemsPerPage={8}
+                            handlePageChange={handlePageChange}
+                            currentPage={currentPage}
+                        />
+                    </div> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SystemMaintenance;
