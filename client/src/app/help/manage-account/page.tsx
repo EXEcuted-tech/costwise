2ae78f54f7a4669e44b335c3a82d30b196cc6e5c
FranzGwapo <franzcasimir.ondiano@gmail.com>
2024-09-13 10:51:28 +0800

    fix: changed header title and added pencil icon on help

--- client/src/app/help/manage-account/page.tsx
+++ client/src/app/help/manage-account/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/header/Header'
 import Link from 'next/link';
+import { FaPencilAlt } from 'react-icons/fa';
 import { GoArrowLeft } from 'react-icons/go';
 import { PiBookOpenText } from "react-icons/pi";
 
@@ -8,15 +9,18 @@ const GettingStartedPage = () => {
     return (
         <div className='bg-cover bg-center items-center justify-center bg-[#FFFAF8] bg-opacity-20 h-[100vh] overflow-hidden'>
             <div>
-                <Header icon={PiBookOpenText} title="User's Management"></Header>
+                <Header icon={PiBookOpenText} title="User's Manual"></Header>
             </div>
             <div className='flex h-[90%] w-[98%] pl-[90px] pt-[15px] -z-50'>
                 <div className='flex flex-col bg-white w-full rounded-xl p-10 drop-shadow-lg'>
-                    <div className='flex flex-row w-full items-center justify-start border-b border-[#ACACAC]'>
+                    <div className='flex flex-row w-full items-center justify-start border-b border-[#ACACAC] gap-[15px]'>
                         <Link href="/help">
                             <GoArrowLeft className="text-primary text-[1em] xl:text-[2em] hover:opacity-75 hover:animate-shrink-in transition ease-in-out duration-200" />
                         </Link>
-                        <p className='flex items-center justify-start text-primary text-[40px] font-bold pl-[15px]'>Manage your account</p>
+                        <p className='flex items-center justify-start text-primary text-[40px] font-bold'>Manage your account</p>
+                        <div className='flex justify-center items-center h-[50%] cursor-pointer transition-colors duration-200 ease-in-out'>
+                            <FaPencilAlt className='w-[30px] h-auto text-primary' />
+                        </div>
                     </div>
                     <div id='scroll-style' className='overflow-y-scroll'>
                         <div className='flex flex-col pt-[50px] text-[30px] text-tertiary'>
