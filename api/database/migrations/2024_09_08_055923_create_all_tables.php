08a3dcdf681a4e171338a7008a30e0b04c76a68e
Katteu <katheamari@gmail.com>
2024-10-17 14:39:50 +0800

    chore: modified database schema

--- api/database/migrations/2024_09_08_055923_create_all_tables.php
+++ api/database/migrations/2024_09_08_055923_create_all_tables.php
@@ -41,7 +41,7 @@ return new class extends Migration {
         // Files Table
         Schema::create('files', function (Blueprint $table) {
             $table->increments('file_id');
-            $table->enum('file_type', ['master_file', 'transactional_file']);
+            $table->enum('file_type', ['master_file', 'transactional_file', 'inventory_file', 'training_file']);
             $table->longText('settings');
             $table->timestamps();
             $table->index(['file_id']);
@@ -80,7 +80,7 @@ return new class extends Migration {
             $table->boolean('is_least_cost')->default(false)->index();
             $table->unsignedInteger('monthYear');
             $table->foreign('fodl_id')->references('fodl_id')->on('fodl')->onDelete('cascade');
-            $table->index(['fg_id','fg_code','rm_cost','fodl_id']);
+            $table->index(['fg_id', 'fg_code', 'rm_cost', 'fodl_id']);
         });
 
         // Formulations Table
@@ -92,7 +92,7 @@ return new class extends Migration {
             $table->longText('material_qty_list');
             $table->timestamps();
             $table->foreign('fg_id')->references('fg_id')->on('finished_goods')->onDelete('cascade');
-            $table->index(['formulation_id','formula_code']);
+            $table->index(['formulation_id', 'formula_code']);
         });
 
         // Material Table
@@ -103,26 +103,13 @@ return new class extends Migration {
             $table->decimal('material_cost', 10, 2);
             $table->string('unit', 10);
             $table->date('date');
-            $table->index(['material_id','material_code', 'material_cost']);
-        });
-
-        // Inventory Table
-        Schema::create('inventory', function (Blueprint $table) {
-            $table->increments('inventory_id');
-            $table->unsignedInteger('material_id');
-            $table->enum('stock_status', ['In Stock', 'Low Stock']);
-            $table->decimal('material_qty', 10, 2);
-            $table->decimal('distributed_qty', 10, 2);
-            $table->decimal('stock_qty', 10, 2);
-            $table->timestamps();
-            $table->foreign('material_id')->references('material_id')->on('materials')->onDelete('cascade');
+            $table->index(['material_id', 'material_code', 'material_cost']);
         });
 
         // Article Table
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('article_id');
             $table->enum('category', ['Manage Account', 'Getting Started', 'Essentials']);
-            $table->string('heading');
             $table->longText('content');
             $table->timestamps();
         });
@@ -145,14 +132,30 @@ return new class extends Migration {
             $table->longText('settings');
             $table->foreign('material_id')->references('material_id')->on('materials')->onDelete('cascade');
             $table->foreign('fg_id')->references('fg_id')->on('finished_goods')->onDelete('cascade');
-            $table->index(['transaction_id','material_id','fg_id']);
+            $table->index(['transaction_id', 'material_id', 'fg_id']);
         });
 
-        Schema::create('models', function (Blueprint $table) {
-            $table->increments('model_id');
-            $table->string('model_name');
-            $table->longText('model_data');
+        // Predictions Table
+        Schema::create('predictions', function (Blueprint $table) {
+            $table->integer('prediction_id', 11)->primary();
+            $table->integer('product_num');
+            $table->string('product_name', 255);
+            $table->decimal('cost', 17, 7);
+            $table->string('monthYear', 50);
             $table->timestamps();
+        });
+
+        // Inventory Table
+        Schema::create('inventory', function (Blueprint $table) {
+            $table->increments('inventory_id');
+            $table->unsignedInteger('material_id');
+            $table->enum('material_category', ['meat_material', 'meat_alternate', 'packaging', 'food_ingredient', 'casing', 'tin_can', 'other']);
+            $table->enum('stock_status', ['In Stock', 'Low Stock']);
+            $table->decimal('purchased_qty', 10, 2);
+            $table->decimal('usage_qty', 10, 2);
+            $table->decimal('total_qty', 10, 2);
+            $table->timestamps();
+            $table->foreign('material_id')->references('material_id')->on('materials')->onDelete('cascade');
         });
     }
 
