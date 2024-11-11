7cb1dab2acaf7596ee9aa46079206e8cc2e92ea2
FranzGwapo <94878281+FranzGwapo@users.noreply.github.com>
2024-11-12 01:45:02 +0800

    fix(web): audit forget password bugs (#120)

--- client/src/app/pass-reset/[token]/page.tsx
+++ client/src/app/pass-reset/[token]/page.tsx
@@ -10,6 +10,7 @@ import Alert from '@/components/alerts/Alert';
 import Spinner from '@/components/loaders/Spinner';
 import ErrorToken from '@/components/modals/ErrorToken';
 import { IoMdInformationCircle } from "react-icons/io";
+import { BsExclamationCircle } from 'react-icons/bs';
 
 
 const PasswordReset = () => {
@@ -42,6 +43,14 @@ const PasswordReset = () => {
             errors.push("Password is required.");
         } else if(password.length < 8) {
           errors.push("Password must be at least 8 characters.");
+        }
+
+        if (!/[A-Z]/.test(password)) {
+          errors.push("Password must contain at least one uppercase letter.");
+        }
+        
+        if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+          errors.push("Password must contain at least one special character.");
         }
       
         if (!passwordConfirmation) {
@@ -98,51 +107,58 @@ const PasswordReset = () => {
         ))}
       </div>
       <div className='flex flex-col animate-pop-out bg-white w-[550px] h-auto pb-[30px] rounded-[20px] px-[10px] gap-3'>
-      <div className='flex justify-center'>
-          <h1 className='font-black text-[30px] mt-[20px]'>Password Change</h1>
-      </div>
-        <div className='flex bg-[#E8E8E8] mx-5 h-10 items-center gap-2 rounded-[10px]'>
-            <IoMdInformationCircle className='text-[30px] ml-[10px]' />
-            <p className='text-[18px] font-bold'>Use at least 8 characters</p>
-        </div>
-      <form onSubmit={handleSubmit} className='flex flex-col w-full px-5 gap-5'>
-          <label>
-          Enter New Password:
-          <input
-              className="bg-white h-12 text-[16px] 2xl:text-[18px] 3xl:text-[20px] w-full px-5 border border-[#B3B3B3] rounded-lg focus:outline"
-              type="password"
-              name="npassword"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="New Password"
-          />
-          </label>
-          <label>
-          Confirm Password:
-          <input
-              className="bg-white h-12 text-[16px] 2xl:text-[18px] 3xl:text-[20px] w-full px-5 border border-[#B3B3B3] rounded-lg focus:outline"
-              type="password"
-              name="cpassword"
-              value={passwordConfirmation}
-              onChange={(e) => setPasswordConfirmation(e.target.value)}
-              placeholder="Confirm Password"
-          />
-          </label>
-
-          <div className='flex w-full justify-center'>
-              <button className='flex justify-center items-center w-[50%] h-[3rem] p-2 text-center text-[1.2em] font-semibold bg-[#00930F] bg-primary text-white rounded-xl hover:bg-[#9c1c1c]'
-              type='submit'
-              >
-              {isLoading? 
-                <>
-                  <Spinner /> 
-                  <p className='ml-2'>Reset Password</p>
-                </>
-                : 
-                'Reset Password'}    
-              </button>
+        <div className='flex justify-center'>
+          <div className='absolute left-[30px] top-[30px] group'>
+              <BsExclamationCircle className='text-[2em] cursor-pointer text-[#c26565] hover:text-[#B22222] hover:animate-shake-tilt transition-all duration-300 ease-in-out 4xl:text-[2em] 3xl:text-[1.8em] 2xl:text-[1.8em] xl:text-[1.6em]' />
+              <div className="absolute w-[300px] h-auto bg-[#FFD3D3] text-[1em] text-[#B22222] p-4 left-[35px] top-[20px] rounded-lg drop-shadow-lg invisible opacity-0 transition-all duration-300 ease-in-out group-hover:visible group-hover:opacity-100 4xl:text-[1em] 3xl:text-[1em] 2xl:text-[0.8em] xl:text-[0.8em]">
+              <p className='text-[18px] font-bold'>Password must contain:</p>
+                <div className='text-[16px] pl-[20px]'>
+                  <p>- at least 8 characters.</p>
+                  <p>- uppercase and lowercase letters.</p>
+                  <p>- special letters &#40;e.g. *&#91;&#93;@#$%*^&+=&#41;</p>
+                </div>
+              </div>
           </div>
-      </form>
+            <h1 className='font-black text-[30px] mt-[20px]'>Password Change</h1>
+        </div>
+        <form onSubmit={handleSubmit} className='flex flex-col w-full px-5 gap-5'>
+            <label>
+            Enter New Password:
+            <input
+                className="bg-white h-12 text-[16px] 2xl:text-[18px] 3xl:text-[20px] w-full px-5 border border-[#B3B3B3] rounded-lg focus:outline"
+                type="password"
+                name="npassword"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="New Password"
+            />
+            </label>
+            <label>
+            Confirm Password:
+            <input
+                className="bg-white h-12 text-[16px] 2xl:text-[18px] 3xl:text-[20px] w-full px-5 border border-[#B3B3B3] rounded-lg focus:outline"
+                type="password"
+                name="cpassword"
+                value={passwordConfirmation}
+                onChange={(e) => setPasswordConfirmation(e.target.value)}
+                placeholder="Confirm Password"
+            />
+            </label>
+
+            <div className='flex w-full justify-center'>
+                <button className='flex justify-center items-center w-[50%] h-[3rem] p-2 text-center text-[1.2em] font-semibold bg-[#00930F] bg-primary text-white rounded-xl hover:bg-[#9c1c1c]'
+                type='submit'
+                >
+                {isLoading? 
+                  <>
+                    <Spinner /> 
+                    <p className='ml-2'>Reset Password</p>
+                  </>
+                  : 
+                  'Reset Password'}    
+                </button>
+            </div>
+        </form>
       </div>
     </>
   )
